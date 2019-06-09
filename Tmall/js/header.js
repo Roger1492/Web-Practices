6baa04b51a871b554e585b8d2816a6007a9a9b52
Roger1492 <rogershen1492@gmail.com>
2019-06-09 23:46:33 +0800

    update Tmall/

---
+++ Tmall/js/header.js
@@ -0,0 +1,27 @@
+// TODO: 轮播图特效
+let slideContainer = document.getElementById('slide-container');
+let slideImg = Array.from(document.querySelectorAll('.slide-wrap div'));
+let slideIndex = -1;
+setInterval(function(e){
+
+  for(let i = 0; i < slideImg.length; i++){
+    slideImg[i].style.display = 'none';
+  }
+
+  if(slideIndex > 4){
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  slideImg[slideIndex].style.display = 'block';
+}, 5000)
+
+// window.onload = function(){
+//   autoRun();
+// }
+// slideContainer.addEventListener('mouseover',function(e){
+//   clearInterval()
+// }, false);
+// slideContainer.addEventListener('mouseout', function(e){
+//   autoRun();
+// }, false);
